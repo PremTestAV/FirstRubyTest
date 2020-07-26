7e5e8e123d890f1fe822f179f9c6fea36a9a4a9c
PremTestAV <premkumar.av@gmail.com>
2020-07-26 12:28:55 -0700

    Adding First File

--- ShopifyFirstTest.rb
+++ ShopifyFirstTest.rb
@@ -10,10 +10,5 @@ class ShopifyFirstTest
     @login_link.click
     @get_started = driver.find_element(:xpath, "//*[@id='body-content']/div[1]/div[2]/div/p/a")
     @get_started.click
-    sleep(5)
-   # @query_box = driver.find_element(:id , 'SignupEmail-7c07')
-   # @query_box.click
-   # @query_box.send_keys ("test@abc.com")
-   # driver.manage.timeouts.implicit_wait = 10
-   # @submit_btn = driver.find_element(:xpath, "//*[@name='button' and text() = 'Start free trial']")
+    sleep(7)
 end

e879a7486c423f4abb2ba4acd1c7b5358e9f4609
PremTestAV <premkumar.av@gmail.com>
2020-07-26 13:21:25 -0700

    Committing Test Unit File

---
+++ ShopifyTestUnit.rb
@@ -0,0 +1,21 @@
+require 'selenium-webdriver'
+require 'test-unit'
+
+class ShopifyTestUnit < Test::Unit::TestCase
+
+    def setup
+        Selenium::WebDriver::Chrome::Service.driver_path = Dir.home + "\\Downloads\\chromedriver_win32\\chromedriver.exe"
+        @driver = Selenium::WebDriver.for :chrome
+        @url =  "https://www.shopify.com/"
+        @driver.manage.timeouts.implicit_wait = 10
+    end
+    def test_shopify
+        @driver.get (@url)
+        @login_link = @driver.find_element(:xpath, "//*[@id='ShopifyMainNav']/ul[2]/li/a")
+        @login_link.click
+        @get_started = @driver.find_element(:xpath, "//*[@id='body-content']/div[1]/div[2]/div/p/a")
+        @get_started.click
+        sleep(7)
+        assert_equal("Sign up Shopify - Free 14-day trial", @driver.title)
+    end
+end
